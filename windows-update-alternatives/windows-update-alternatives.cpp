e60e9bfbe5ccb87974503aec2444e23068cd3ad0
ssssdl <1463620240@qq.com>
2019-10-29 18:36:30 +0800

    Add Readme

--- windows-update-alternatives/windows-update-alternatives.cpp
+++ windows-update-alternatives/windows-update-alternatives.cpp
@@ -1,13 +1,101 @@
 ﻿// windows-update-alternatives.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
+// 首先设置环境变量到某个链接路径(希望可以在install中实现)
+// 要有一个配置文件，里面标清注册了那些应用，目前是什么版本，一共有那些版本
+// 运行文件，接收命令行参数和原版工具一致，有初始注册组件(install)，删除组件(remove)和配置组件(config)功能
 
+// 尽可能的还原原版
 #include "pch.h"
 #include <iostream>
+#include <stdlib.h>
 
-int main()
+using namespace std;
+
+bool menu(string argv1);	//菜单
+void help();				//打印--help
+
+/**
+ * @title main
+ * @author ['ssssdl']
+ * @dev 主函数，接收命令行参数，提交给menu处理分支
+ * @param argc 命令行参数个数
+ * @param argv 命令行参数内容
+ * @return 0
+ */
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n"; 
+	cout << argc << endl;  //参数个数
+	menu(argv); //除了执行文件的第一个参数 --help
+    cout << "Hello World!\n"; 
+	return 0;
 }
+
+/**
+ * @title menu
+ * @author ['ssssdl']
+ * @dev 菜单函数，处理命令行参数交给相关模块处理  //这里面应该是两个命令行参数都需要传过来
+ * @param argv1 命令行参数内容
+ * @return bool 返回是否成功
+ */
+bool menu(char* argv[]) {
+	//本来想switch 太菜了  秀不起来
+	if (argv[1] == "--install" || argv[1] == "-i") {
+		cout << "注册组件" << endl;
+		return true;
+	}
+	else if(argv[1] == "--remove" || argv[1] == "-r") {
+		cout << "删除已经注册的组件" << endl;
+		return true;
+	}
+	else if (argv[1] == "--config" || argv[1] == "-c") {
+		cout << "删除已经注册的组件" << endl;
+		return true;
+	}
+	else if (argv[1] == "--set" || argv[1] == "-s") {
+		//update-alternatives --set python /usr/bin/python3.6
+		cout << "直接设置路径" << endl;
+		return true;
+	}
+	else if (argv[1] == "--help" || argv[1] == "-h") {
+		help();
+		return true;
+	}
+	return false;
+}
+
+/**
+ * @title help
+ * @author ['ssssdl']
+ * @dev 处理--help||-h，打印help
+ * @param null
+ * @return void
+ */
+
+void help() {
+	cout << "windows-update-alternatives --set python D:\python27 \t 直接设置路径python路径为D:\python27" << endl;
+}
+
+/**
+ * @title set
+ * @author ['ssssdl']
+ * @dev 处理--set||-s
+ * @param app 要设置路径的app
+ * @param path 要设置的路径
+ * @return bool 返回是否成功
+ */
+bool set(string app, string *path) {
+	// 先查询app是否注册，没注册的话注册（设置环境变量，默认软连接到程序目录，建立软连接，添加记录，设置优先级默认10）--to do
+	// 已经注册的app，查询软连接到的地址，删除改地址重新创建 --doing
+	// 例子：
+	string appName = app;	//php
+	//利用appname查询appFromPath，假设结果如下
+	string appFromPath = "D:\\wamp64\\bin\\php\\php";  //比如查询结果是D:\wamp64\bin\php\php
+	char appToPath = path;	//新设置的路径
+	system("rmdir " + appFromPath);
+	system("mklink / D " + appFromPath + " " + appToPath);
+	return false;
+}
+
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
